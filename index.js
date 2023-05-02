207e6c56679a41160233a140c425f527f4f4dfe0
Vladimir RM <shvdrummer@gmail.com>
2023-05-02 14:06:57 +0300

    i mongoose

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import  express from "express";
 const PORT = 5000
 
 const app = express()
+app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.post('/',(req,res)=>{
+    console.log(req.body)
     res.status(200).json('Сервер работает')
 })
 
